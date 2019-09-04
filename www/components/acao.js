afe3e8f1695b3f0c966f5c29058efd3a8a08685e
Carlosetec <carloseduardoetecab@gmail.com>
2019-09-05 04:12:56 +0900

    Acho que eu terminei

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,5 +1,41 @@
-var p1 = window.document.getElementById('bt')
-p1.style.marginTop = "70px";
+$(document).on("click","#a",function(){
 
-window.document.write("Java Script");
+function checkConnection() {
+    var networkState = navigator.connection.type;
+    if(navigator.connection.type == Connection.NONE){
+      alert('No network connection')
+      navigator.notification.beep(3);
+      navigator.vibrate(3000);
+    }
+}
+
+checkConnection();
+
+navigator.notification.beep(1);
+
+var onSuccess = function(position) {
+
+L.mapquest.key = 'tJ9q9yMZSYYKGxQfp4LJRRXyRC6ERcwR';
+
+// 'map' refers to a <div> element with the ID map
+L.mapquest.map('map', {
+  center: [position.coords.latitude, position.coords.longitude],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 15,
+});
+};
+
+function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+});
+
+
+
+
+
 
